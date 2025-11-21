8b88eff63d0647f82781742f6829925af0caff13
Nguyên Tỷ <phamnguyenty0991@gmail.com>
2025-11-21 10:36:47 +0700

    1111

--- ThuNghiemGit/Form1.cs
+++ ThuNghiemGit/Form1.cs
@@ -24,7 +24,7 @@ namespace ThuNghiemGit
             int b = 20;
             Console.WriteLine("BBBccccBBB");
             Console.WriteLine("CCCCCC");
-            Console.WriteLine("AAAAAAAAAA");
+            Console.WriteLine("AAAAAAAAAAccc");
         }
     }
 }
